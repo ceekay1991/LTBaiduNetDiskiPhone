e23a27c1b6507c33b3c3eb4af6ce293f0b2335bc
ceekay1991 <374993061@qq.com>
2019-12-17 18:31:27 +0800

    支持百度网盘10.0.120版本

--- LTBaiduNetDiskiPhonePod/LTBaiduNetDiskiPhonePod.m
+++ LTBaiduNetDiskiPhonePod/LTBaiduNetDiskiPhonePod.m
@@ -12,59 +12,59 @@
 #import <CaptainHook/CaptainHook.h>
 #import <UIKit/UIKit.h>
 
-CHDeclareClass(BDUser)
+CHDeclareClass(BDPanAccountInfo)
 
-CHOptimizedClassMethod0(self, BOOL, BDUser, isSVIP){
+CHOptimizedMethod0(self, BOOL, BDPanAccountInfo, isSVIP){
     return YES;
 }
-CHOptimizedClassMethod0(self, BOOL, BDUser, svipExpireTime){
+CHOptimizedMethod0(self, BOOL, BDPanAccountInfo, svipExpireTime){
     return [[NSDate dateWithTimeIntervalSinceNow:1 * 365 * 24 * 60 * 60] timeIntervalSince1970];
 }
 
+void modifyUserInfoToVip(){
+    NSString *userId = ((id (*)(id, SEL))objc_msgSend)(objc_getClass("BDNCAccountInfo"),@selector(userId));
+       NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+       NSDictionary *originDic = [ud objectForKey:userId];
+       NSLog(@"%@ == %@",userId,originDic);
+       
+       NSTimeInterval expireTime = [[NSDate dateWithTimeIntervalSinceNow:1 * 365 * 24 * 60 * 60] timeIntervalSince1970];
+       NSString *expireTimeStr = [NSString stringWithFormat:@"%f",expireTime];
+       NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:originDic];
+       [dic setValue:@YES forKey:@"isSVipUser"];
+       [dic setValue:expireTimeStr forKey:@"SVipExpireTime"];
+       [ud setObject:dic forKey:userId];
+}
 CHDeclareClass(BDPanCloudFileListViewController)
 
 CHOptimizedMethod1(self, void, BDPanCloudFileListViewController, viewWillAppear, BOOL, arg1){
     CHSuper1(BDPanCloudFileListViewController, viewWillAppear, arg1);
-    
-    NSString *userId = ((id (*)(id, SEL))objc_msgSend)(objc_getClass("BDUser"),@selector(userId));
-    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-    NSDictionary *originDic = [ud objectForKey:userId];
-    NSLog(@"%@ == %@",userId,originDic);
-    
-    NSTimeInterval expireTime = [[NSDate dateWithTimeIntervalSinceNow:1 * 365 * 24 * 60 * 60] timeIntervalSince1970];
-    NSString *expireTimeStr = [NSString stringWithFormat:@"%f",expireTime];
-    NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:originDic];
-    [dic setValue:@YES forKey:@"isSVipUser"];
-    [dic setValue:expireTimeStr forKey:@"SVipExpireTime"];
-    [ud setObject:dic forKey:userId];
+    modifyUserInfoToVip();
 }
 
 CHDeclareClass(DownTransListViewController)
 
 CHOptimizedMethod1(self, void, DownTransListViewController, viewWillAppear, BOOL, arg1){
     CHSuper1(DownTransListViewController, viewWillAppear, arg1);
-    NSString *userId = ((id (*)(id, SEL))objc_msgSend)(objc_getClass("BDUser"),@selector(userId));
-    
-    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-    NSDictionary *originDic = [ud objectForKey:userId];
-    NSLog(@"%@ == %@",userId,originDic);
-    
-    NSTimeInterval expireTime = [[NSDate dateWithTimeIntervalSinceNow:1 * 365 * 24 * 60 * 60] timeIntervalSince1970];
-    NSString *expireTimeStr = [NSString stringWithFormat:@"%f",expireTime];
-    NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithDictionary:originDic];
-    [dic setValue:@YES forKey:@"isSVipUser"];
-    [dic setValue:expireTimeStr forKey:@"SVipExpireTime"];
-    [ud setObject:dic forKey:userId];
+     modifyUserInfoToVip();
+}
+
+CHDeclareClass(SettingViewController)
+
+CHOptimizedMethod1(self, void, SettingViewController, viewWillAppear, BOOL, arg1){
+    CHSuper1(SettingViewController, viewWillAppear, arg1);
+    modifyUserInfoToVip();
 }
 
 
 CHConstructor{
-    CHLoadLateClass(BDUser);
+    CHLoadLateClass(BDPanAccountInfo);
     CHLoadLateClass(BDPanCloudFileListViewController);
     CHLoadLateClass(DownTransListViewController);
-    CHClassHook0(BDUser, isSVIP);
-    CHClassHook0(BDUser, svipExpireTime);
+    CHClassHook0(BDPanAccountInfo, isSVIP);
+    CHClassHook0(BDPanAccountInfo, svipExpireTime);
     
     CHHook1(BDPanCloudFileListViewController, viewWillAppear);
     CHHook1(DownTransListViewController, viewWillAppear);
+    CHHook1(SettingViewController, viewWillAppear);
 }
+
